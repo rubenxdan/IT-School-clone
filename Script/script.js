995ac0802450548a68412a3f31a018d3fc92dae4
Ruben Lucaci <lucaciruben@gmail.com>
2022-08-26 16:41:20 +0300

    sliding-animation-added-to-collapsible

--- Script/script.js
+++ Script/script.js
@@ -24,12 +24,11 @@ var i;
 
 for (i = 0; i < collapse.length; i++) {
   collapse[i].addEventListener("click", function () {
-    this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
